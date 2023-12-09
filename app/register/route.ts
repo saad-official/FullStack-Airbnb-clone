3d830438b24ee8d5085fa373d61d1de0a8d02e5a
Sadi07 <saad.khan@zortik.com>
2023-12-09 18:57:33 +0500

    configure next-auth

---
+++ app/register/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "../libs/prismadb";
+import bcrypt from "bcrypt";
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const { email, name, password } = body;
+
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
+}
