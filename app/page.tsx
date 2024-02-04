0886c785a482f9d0b799961fbcb6436ce8a1843e
Sadi07 <saad.khan@zortik.com>
2024-02-04 22:03:55 +0500

    dynamic server issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic'
 import getCurrentUser from "./actions/getCurrentUser";
 import getListings, { IListingParams } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
